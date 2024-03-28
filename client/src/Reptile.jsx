483c38dd5dead4dea76e0dae9c420e5f7f6beb24
Jonathan Gates <jonathangates33@gmail.com>
2024-03-28 12:25:37 -0600

    removed commented out dead code and made edit reptile button stay on modal show

--- client/src/Reptile.jsx
+++ client/src/Reptile.jsx
@@ -2,21 +2,18 @@ import { useState, useEffect } from "react";
 import { useApi } from "../utils/use_api";
 import { useParams } from "react-router-dom";
 import { EditReptileModal } from "./components/EditReptileModal/EditReptileModal";
-import {AddFeedingModal} from "./components/AddFeedingModal/AddFeedingModal";
-import './Reptile.css';
-
+import { AddFeedingModal } from "./components/AddFeedingModal/AddFeedingModal";
+import "./Reptile.css";
 
 export const Reptile = () => {
   const { reptileId } = useParams(); // Assuming you're using react-router and reptileId is in the URL
   const api = useApi();
 
   const [reptile, setReptile] = useState(null);
-  const [isUpdatingReptile, setIsUpdatingReptile] = useState(false);;
+  const [isUpdatingReptile, setIsUpdatingReptile] = useState(false);
 
   const [feedings, setFeedings] = useState([]);
   const [isCreatingFeeding, setIsCreatingFeeding] = useState(false);
-  // const [newFeedingFoodItem, setNewFeedingFoodItem] = useState("");
-  // const [newFeedingDate, setNewFeedingDate] = useState("");
 
   const [husbandryRecords, setHusbandryRecords] = useState([]);
   const [isCreatingHusbandryRecord, setIsCreatingHusbandryRecord] =
@@ -62,29 +59,6 @@ export const Reptile = () => {
 
     fetchData();
   }, [api, reptileId]);
-
-  // Handlers for creating new records
-  // const handleCreateFeeding = async (e) => {
-  //   e.preventDefault(); // Prevent default form submission behavior
-
-  //   try {
-  //     const feedingInfo = {
-  //       reptileId: reptileId, // Ensure this is passed correctly to your API
-  //       foodItem: newFeedingFoodItem,
-  //       date: newFeedingDate,
-  //     };
-  //     const res = await api.post(`/feedings`, feedingInfo);
-  //     setIsCreatingFeeding(false); // Close the form
-  //     setNewFeedingFoodItem(""); // Reset the form fields
-  //     setNewFeedingDate("");
-  //     // Optionally, fetch the updated list of feedings here to reflect the new addition
-  //     if (res.feeding) {
-  //       setFeedings((prev) => [...prev, res.feeding]);
-  //     }
-  //   } catch (error) {
-  //     console.error("Failed to create new feeding:", error);
-  //   }
-  // };
 
   const handleCreateHusbandryRecord = async (e) => {
     e.preventDefault(); // Prevent default form submission behavior
@@ -146,208 +120,257 @@ export const Reptile = () => {
 
   return (
     <>
-    <div className="reptile-container">
-    <div className="container">
-    <div className="row">
-    <div className="col-md-3">
-  <h1>Reptile Details</h1>
-  {/* Edit Reptile Details Button */}
-  {!isUpdatingReptile && (
-    <button onClick={() => setIsUpdatingReptile(true)} className="btn btn-primary mb-3">
-      Edit Reptile Details
-    </button>
-  )}
+      <div className="reptile-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <h1>Reptile Details</h1>
+              {/* Edit Reptile Details Button */}
+              <button
+                onClick={() => setIsUpdatingReptile(true)}
+                className="btn btn-primary mb-3"
+              >
+                Edit Reptile Details
+              </button>
+              {/* Display reptile details */}
+              {reptile && (
+                <div>
+                  <p>Name: {reptile.name}</p>
+                  <p>Sex: {reptile.sex}</p>
+                  <p>Species: {reptile.species}</p>
+                  {/* Add more reptile details as needed */}
+                </div>
+              )}
 
-  {/* Display reptile details */}
-  {reptile && (
-    <div>
-      <p>Name: {reptile.name}</p>
-      <p>Sex: {reptile.sex}</p>
-      <p>Species: {reptile.species}</p>
-      {/* Add more reptile details as needed */}
-    </div>
-  )}
+              {/* Update Reptile Details Form */}
+              <EditReptileModal
+                show={isUpdatingReptile}
+                onHide={() => setIsUpdatingReptile(false)}
+                reptile={reptile}
+                setReptile={setReptile}
+              />
+            </div>
+            <div className="col-md-3">
+              <h2>Feedings</h2>
+              <AddFeedingModal
+                show={isCreatingFeeding}
+                onHide={() => setIsCreatingFeeding(false)}
+                setFeedings={setFeedings}
+              />
+              <button
+                onClick={() => setIsCreatingFeeding(true)}
+                className="btn btn-primary"
+              >
+                Add New Feeding
+              </button>
 
-  {/* Update Reptile Details Form */}
-  <EditReptileModal show={isUpdatingReptile} onHide={() => setIsUpdatingReptile(false)} reptile={reptile} setReptile={setReptile}/>
-</div>
-    <div className="col-md-3">
-      <h2>Feedings</h2>
-        <AddFeedingModal show={isCreatingFeeding} onHide={() => setIsCreatingFeeding(false)}  setFeedings={setFeedings}/>
-        <button onClick={() => setIsCreatingFeeding(true)} className="btn btn-primary">
-          Add New Feeding
-        </button>
+              <ul>
+                {feedings &&
+                  feedings.map((feeding) => (
+                    <li key={feeding.id}>
+                      Food: {feeding.foodItem} - Date: {feeding.createdAt}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <h2>Husbandry Records</h2>
+              {isCreatingHusbandryRecord ? (
+                <form onSubmit={handleCreateHusbandryRecord} className="mt-3">
+                  <h3>Create New Husbandry Record</h3>
+                  <div className="mb-3">
+                    <label htmlFor="length" className="form-label">
+                      Length:
+                    </label>
+                    <input
+                      id="length"
+                      type="number"
+                      className="form-control"
+                      step="0.01"
+                      value={newLength}
+                      onChange={(e) => setNewLength(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="weight" className="form-label">
+                      Weight:
+                    </label>
+                    <input
+                      id="weight"
+                      type="number"
+                      className="form-control"
+                      step="0.01"
+                      value={newWeight}
+                      onChange={(e) => setNewWeight(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="temperature" className="form-label">
+                      Temperature:
+                    </label>
+                    <input
+                      id="temperature"
+                      type="number"
+                      className="form-control"
+                      step="0.01"
+                      value={newTemperature}
+                      onChange={(e) => setNewTemperature(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="humidity" className="form-label">
+                      Humidity:
+                    </label>
+                    <input
+                      id="humidity"
+                      type="number"
+                      className="form-control"
+                      step="0.01"
+                      value={newHumidity}
+                      onChange={(e) => setNewHumidity(e.target.value)}
+                    />
+                  </div>
+                  <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="submit" className="btn btn-primary me-2">
+                      Add Record
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={() => setIsCreatingHusbandryRecord(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              ) : (
+                <button
+                  onClick={() => setIsCreatingHusbandryRecord(true)}
+                  className="btn btn-primary"
+                >
+                  Add New Husbandry Record
+                </button>
+              )}
+              <ul>
+                {husbandryRecords &&
+                  husbandryRecords.map((record) => (
+                    <li key={record.id}>
+                      Length: {record.length} - Weight: {record.weight} - Temp:{" "}
+                      {record.temperature} - Humidity: {record.humidity}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <h2>Schedules</h2>
+              {isCreatingSchedule ? (
+                <form onSubmit={handleCreateSchedule} className="mt-3">
+                  <h3>Create New Schedule</h3>
+                  <div className="mb-3">
+                    <label htmlFor="scheduleType" className="form-label">
+                      Type:
+                    </label>
+                    <select
+                      id="scheduleType"
+                      className="form-select"
+                      value={newScheduleType}
+                      onChange={(e) => setNewScheduleType(e.target.value)}
+                    >
+                      <option value="feed">Feed</option>
+                      <option value="record">Record</option>
+                      <option value="clean">Clean</option>
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="description" className="form-label">
+                      Description:
+                    </label>
+                    <input
+                      id="description"
+                      type="text"
+                      className="form-control"
+                      value={newDescription}
+                      onChange={(e) => setNewDescription(e.target.value)}
+                    />
+                  </div>
+                  <fieldset className="mb-3">
+                    <legend className="form-label">Days:</legend>
+                    {Object.keys(days).map((day) => (
+                      <div key={day} className="form-check">
+                        <input
+                          type="checkbox"
+                          className="form-check-input"
+                          id={`day-${day}`}
+                          checked={days[day]}
+                          onChange={() =>
+                            setDays({ ...days, [day]: !days[day] })
+                          }
+                        />
+                        <label
+                          htmlFor={`day-${day}`}
+                          className="form-check-label"
+                        >
+                          {day.charAt(0).toUpperCase() + day.slice(1)}
+                        </label>
+                      </div>
+                    ))}
+                  </fieldset>
+                  <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="submit" className="btn btn-primary me-2">
+                      Add Schedule
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={() => setIsCreatingSchedule(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              ) : (
+                <button
+                  onClick={() => setIsCreatingSchedule(true)}
+                  className="btn btn-primary"
+                >
+                  Add New Schedule
+                </button>
+              )}
+              <ul>
+                {schedules &&
+                  schedules.map((schedule) => {
+                    // Extracting only the days that are true and joining them into a string
+                    const activeDays = Object.entries(schedule)
+                      .filter(
+                        ([key, value]) =>
+                          value === true &&
+                          [
+                            "monday",
+                            "tuesday",
+                            "wednesday",
+                            "thursday",
+                            "friday",
+                            "saturday",
+                            "sunday",
+                          ].includes(key)
+                      )
+                      .map(
+                        ([key]) => key.charAt(0).toUpperCase() + key.slice(1)
+                      ) // Capitalize the first letter of each day
+                      .join(", ");
 
-      <ul>
-        {feedings &&
-          feedings.map((feeding) => (
-            <li key={feeding.id}>
-              Food: {feeding.foodItem} - Date: {feeding.createdAt}
-            </li>
-          ))}
-      </ul>
-    </div>
-    <div className="col-md-3">
-      <h2>Husbandry Records</h2>
-      {isCreatingHusbandryRecord ? (
-  <form onSubmit={handleCreateHusbandryRecord} className="mt-3">
-    <h3>Create New Husbandry Record</h3>
-    <div className="mb-3">
-      <label htmlFor="length" className="form-label">Length:</label>
-      <input
-        id="length"
-        type="number"
-        className="form-control"
-        step="0.01"
-        value={newLength}
-        onChange={(e) => setNewLength(e.target.value)}
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="weight" className="form-label">Weight:</label>
-      <input
-        id="weight"
-        type="number"
-        className="form-control"
-        step="0.01"
-        value={newWeight}
-        onChange={(e) => setNewWeight(e.target.value)}
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="temperature" className="form-label">Temperature:</label>
-      <input
-        id="temperature"
-        type="number"
-        className="form-control"
-        step="0.01"
-        value={newTemperature}
-        onChange={(e) => setNewTemperature(e.target.value)}
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="humidity" className="form-label">Humidity:</label>
-      <input
-        id="humidity"
-        type="number"
-        className="form-control"
-        step="0.01"
-        value={newHumidity}
-        onChange={(e) => setNewHumidity(e.target.value)}
-      />
-    </div>
-    <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-      <button type="submit" className="btn btn-primary me-2">Add Record</button>
-      <button type="button" className="btn btn-secondary" onClick={() => setIsCreatingHusbandryRecord(false)} >
-        Cancel
-      </button>
-    </div>
-  </form>
-      ) : (
-        <button onClick={() => setIsCreatingHusbandryRecord(true)} className="btn btn-primary">
-          Add New Husbandry Record
-        </button>
-      )}
-      <ul>
-        {husbandryRecords &&
-          husbandryRecords.map((record) => (
-            <li key={record.id}>
-              Length: {record.length} - Weight: {record.weight} - Temp:{" "}
-              {record.temperature} - Humidity: {record.humidity}
-            </li>
-          ))}
-      </ul>
-    </div>
-    <div className="col-md-3">
-      <h2>Schedules</h2>
-      {isCreatingSchedule ? (
-  <form onSubmit={handleCreateSchedule} className="mt-3">
-    <h3>Create New Schedule</h3>
-    <div className="mb-3">
-      <label htmlFor="scheduleType" className="form-label">Type:</label>
-      <select
-        id="scheduleType"
-        className="form-select"
-        value={newScheduleType}
-        onChange={(e) => setNewScheduleType(e.target.value)}
-      >
-        <option value="feed">Feed</option>
-        <option value="record">Record</option>
-        <option value="clean">Clean</option>
-      </select>
-    </div>
-    <div className="mb-3">
-      <label htmlFor="description" className="form-label">Description:</label>
-      <input
-        id="description"
-        type="text"
-        className="form-control"
-        value={newDescription}
-        onChange={(e) => setNewDescription(e.target.value)}
-      />
-    </div>
-    <fieldset className="mb-3">
-      <legend className="form-label">Days:</legend>
-      {Object.keys(days).map((day) => (
-        <div key={day} className="form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id={`day-${day}`}
-            checked={days[day]}
-            onChange={() => setDays({ ...days, [day]: !days[day] })}
-          />
-          <label htmlFor={`day-${day}`} className="form-check-label">
-            {day.charAt(0).toUpperCase() + day.slice(1)}
-          </label>
+                    return (
+                      <li key={schedule.id}>
+                        Type: {schedule.type} - Description:{" "}
+                        {schedule.description} - Days: {activeDays}
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
         </div>
-      ))}
-    </fieldset>
-    <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-      <button type="submit" className="btn btn-primary me-2">Add Schedule</button>
-      <button type="button" className="btn btn-secondary" onClick={() => setIsCreatingSchedule(false)}>
-        Cancel
-      </button>
-    </div>
-  </form>
-      ) : (
-        <button onClick={() => setIsCreatingSchedule(true)} className="btn btn-primary">
-          Add New Schedule
-        </button>
-      )}
-      <ul>
-        {schedules &&
-          schedules.map((schedule) => {
-            // Extracting only the days that are true and joining them into a string
-            const activeDays = Object.entries(schedule)
-              .filter(
-                ([key, value]) =>
-                  value === true &&
-                  [
-                    "monday",
-                    "tuesday",
-                    "wednesday",
-                    "thursday",
-                    "friday",
-                    "saturday",
-                    "sunday",
-                  ].includes(key)
-              )
-              .map(([key]) => key.charAt(0).toUpperCase() + key.slice(1)) // Capitalize the first letter of each day
-              .join(", ");
-
-            return (
-              <li key={schedule.id}>
-                Type: {schedule.type} - Description: {schedule.description} -
-                Days: {activeDays}
-              </li>
-            );
-          })}
-      </ul>
       </div>
-    </div>
-    </div>
-    </div>
     </>
   );
 };
